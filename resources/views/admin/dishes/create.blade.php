33e57b90618cbe4fa63cbbf7e03898eb0d5374f8
maichol <msibiriu@gmail.com>
2021-02-26 19:27:26 +0100

    inizio style admin/restaurants/create

--- resources/views/admin/dishes/create.blade.php
+++ resources/views/admin/dishes/create.blade.php
@@ -1,6 +1,6 @@
 @extends('layouts.app')
 @section('content')
-<h1>Crea un nuovo piatto</h1>
+<h1 class="text-center">Crea un nuovo piatto</h1>
 
   @if ($errors->any())
     <ul>
@@ -16,11 +16,11 @@
     @csrf
     @method('POST')
 
-    <div>
+    <div class="form-group">
       <label for="name">Nome</label>
       <input type="text" id="name" name="name" value="{{ old('name') }}">
     </div>
-    <div>
+    <div class="form-group">
       <label for="price">Prezzo</label>
       <input type="number" min="0" max="999" id="price" name="price" value="{{ old('price') }}">
     </div>
@@ -28,7 +28,7 @@
       <label for="image">Immagine</label>
       <input type="file" id="image" name="image" accept="image/*">
     </div>
-    <div>
+    <div class="form-group">
       <label for="description">Descrizione</label>
       <textarea id="description" name="description">{{ old('description') }}</textarea>
     </div>
--- resources/views/admin/restaurants/create.blade.php
+++ resources/views/admin/restaurants/create.blade.php
@@ -1,6 +1,6 @@
 @extends('layouts.app')
 @section('content')
-<h1>Crea nuovo ristorante</h1>
+<h1 class="text-center">Crea nuovo ristorante</h1>
     @if ($errors->any())
     <ul>
         @foreach ($errors->all() as $error)
@@ -11,36 +11,52 @@
     </ul>
     <hr>
     @endif
-    <form action="{{ route('admin.restaurants.store') }}" method="POST" enctype="multipart/form-data">
-        @csrf
-        @method("POST")
 
-        <div>
-            <label for="name">Nome Ristorante</label>
-            <input type="text" name="name" id="name" value="{{ old('name') }}">
-        </div>
-        <div>
-            <label for="address">Indirizzo</label>
-            <input type="text" name="address" id="address" value="{{ old('address') }}">
-        </div>
-        <div>
-            <label for="vat_num">Partita IVA</label>
-            <input type="text" name="vat_num" id="vat_num" value="{{ old('vat_num') }}">
-        </div>
-        <div>
-            <label for="phone">Numero di Telefono</label>
-            <input type="text" name="phone" id="phone" value="{{ old('phone') }}">
-        </div>
-        <div>
-            <label for="img_logo">Logo Ristorante</label>
-            <input type="file" name="image_logo" id="img_logo" accept="image/*">
-        </div>
-        <div>
-            @foreach ($categories as $category)
-            <input type="checkbox" name="categories[]" id="{{ $category->name }}" value = "{{ $category->id }}"><label for="{{ $category->name }}">{{ $category->name }}</label><br>            
-            @endforeach
-        </div>
+    <div class="container">
+        <form action="{{ route('admin.restaurants.store') }}" method="POST" enctype="multipart/form-data" class="d-flex">
+            @csrf
+            @method("POST")
 
-        <input type="submit" value="Crea Ristorante">
-    </form>
+            <div class="input">
+                <div class="form-group">
+                    <label for="name">Nome Ristorante</label>
+                    <input class="form-control" type="text" name="name" id="name" value="{{ old('name') }}">
+                </div>
+                <div class="form-group">
+                    <label for="address">Indirizzo</label>
+                    <input class="form-control" type="text" name="address" id="address" value="{{ old('address') }}">
+                </div>
+                <div class="form-group">
+                    <label for="vat_num">Partita IVA</label>
+                    <input class="form-control" type="text" name="vat_num" id="vat_num" value="{{ old('vat_num') }}">
+                </div>
+                <div class="form-group">
+                    <label for="phone">Numero di Telefono</label>
+                    <input class="form-control" type="text" name="phone" id="phone" value="{{ old('phone') }}">
+                </div>
+                <div class="form-group">
+                    <label for="img_logo">Logo Ristorante</label>
+                    <input type="file" name="image_logo" id="img_logo" accept="image/*">
+                </div>
+            </div>
+
+            <div class="all-categories">
+
+                {{-- immagine logo nuovo ristorante --}}
+                {{-- <img src="{{ asset('storage/') }}" alt=""> --}}
+
+                <h3 class="text-center">TIPOLOGIE</h3>
+                <div class="box-categories form-group d-flex">
+                    @foreach ($categories as $category)
+                    <div class="category">
+                        <input type="checkbox" name="categories[]" id="{{ $category->name }}" value = "{{ $category->id }}">
+                        <label for="{{ $category->name }}">{{ $category->name }}</label><br>            
+                    </div>
+                    @endforeach
+                </div>
+        
+                <input type="submit" value="Crea Ristorante">
+            </div>
+        </form>
+    </div>
 @endsection
