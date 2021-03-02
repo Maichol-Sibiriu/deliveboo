452fb7ffa808fbebed0764f652e4938a74ed2848
Massimiliano Ambrogio <maxi.ambrogio@gmail.com>
2021-03-02 19:51:56 +0100

    fix create, edit class create-dishes, edit-dishes

--- resources/views/admin/dishes/create.blade.php
+++ resources/views/admin/dishes/create.blade.php
@@ -18,23 +18,23 @@
 
     <div class="form-group">
       <label for="name" class="name">Nome</label>
-      <input type="text" class="form-control create" id="name" name="name" value="{{ old('name') }}">
+      <input type="text" class="form-control create-dishes" id="name" name="name" value="{{ old('name') }}">
     </div>
     <div class="form-group">
       <label for="price" class="price">Prezzo</label>
-      <input type="number" class="form-control create" min="0" max="999" id="price" name="price" value="{{ old('price') }}">
+      <input type="number" class="form-control create-dishes" min="0" max="999" id="price" name="price" value="{{ old('price') }}">
     </div>
     <div>
       <label for="image" class="img">Immagine</label>
-      <input type="file" class="form-control create" id="image" name="image" accept="image/*">
+      <input type="file" class="form-control create-dishes" id="image" name="image" accept="image/*">
     </div>
     <div class="form-group">
       <label for="description" class="description1">Descrizione</label>
-      <textarea id="description" class="form-control create mb-2" name="description">{{ old('description') }}</textarea>
+      <textarea id="description" class="form-control create-dishes mb-2" name="description">{{ old('description') }}</textarea>
     </div>
     <div>
       <label for="typology" class="typology">Tipologia</label>
-      <select name="typology" class="form-control create" id="typology">
+      <select name="typology" class="form-control create-dishes" id="typology">
         @foreach($typologies as $typology)
           <option value="{{ $typology }}">{{ $typology }}</option>
         @endforeach
@@ -50,6 +50,6 @@
       <input type="checkbox" id="available" name="available">
     </div>
 
-    <input type="submit1" class="btn btn-submit submit1 form-control create" value="Crea piatto">
+    <input type="submit1" class="btn btn-submit submit1 form-control create-dishes" value="Crea piatto">
   </form>
 @endsection
