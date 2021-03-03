177f7e2f1eb78dc27a06c64183ff96449f570f92
Osama Paiola <paiolaosama@gmail.com>
2021-03-03 13:01:36 +0100

    dashboard sidebar

--- resources/views/admin/home.blade.php
+++ resources/views/admin/home.blade.php
@@ -1,15 +1,29 @@
 @extends('layouts.app')
 
 @section('content')
-<main class="main-admin-home">
+<main class="main-admin-home d-flex">
 
-  <!-- <div class="sidebar">
-    content sidebar
-  </div> -->
+  <div class="sidebar ">
+    <h6 class="text-center">CREA / MODIFICA RISTORANTE</h6>
 
-  <div class="container">
+    <div class="side-menu">
+      <div class="btn-group">
+        <button type="button" class="btn btn-menu dropdown-toggle" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+          MENU
+        </button>
+        <div class="dropdown-menu box-menu">
+          <a class="dropdown-item" href="#">Visualizza Menu</a>
+          <a class="dropdown-item" href="#">Aggiungi nuovo piatto</a>
+        </div>
+      </div>
+    </div>
 
+    <div class="side-ordini">
 
+    </div>
+  </div>
+
+  <div class="content ">
     @if (session('saved'))
     <div class="alert alert-success">
       <p>E' stato creato un ristorante dal nome: {{session('saved')}}</p>
@@ -32,84 +46,46 @@
     </div>
 
     <!-- content titolo + info ristorante -->
-    <div class="cont-home">
+    <div class="info-risto">
       <h2>{{ $restaurant->name }}</h2>
+      <p class="address">
+        <i class="fas fa-map"></i> : {{$restaurant->address}}
+      </p>
+      <p class="p-iva"> P.IVA : {{$restaurant->vat_num}}
+      </p>
+      <p class="phone">
+        <i class="fas fa-phone"></i>: {{$restaurant->phone}}
+      </p>
 
-      <div class="info-rest">
-        <p class="address">
-          <i class="fas fa-map"></i>
-          : {{$restaurant->address}}</p>
-          <p class="p-iva"> P.IVA : {{$restaurant->vat_num}} </p>
-          <p class="phone"> <i class="fas fa-phone"></i>: {{$restaurant->phone}} </p>
-        </div>
+      <div class="btns d-flex mt-5 mb-4">
+        <a class="btn ordini text-center" href="{{route('admin.orders.index')}}">ORDINI
+        </a>
+        <a class="btn menu text-center" href="{{ route('admin.dishes.index') }}">
+          MENU
+        </a>
+        <a class="btn edit text-center" href="{{ route('admin.restaurants.edit', $restaurant->id) }}">MODIFICA RISTORANTE</a>
+      </div>
+
+      <!-- form cancellazione ristorante -->
+      <form action="{{ route('admin.restaurants.destroy', $restaurant->id) }}" method="POST">
+        @csrf
+        @method('DELETE')
+        <input class="btn delete" type="submit" value="Cancella Ristorante">
+      </form>
+      @empty
+      <div class="empty">
+        <a class="btn create text-center"   href="{{route('admin.restaurants.create') }}">
+          CREA IL TUO RISTORNATE
+        </a>
+      </div>
+
     </div>
 
-    <div class="btns d-flex mt-5 mb-4">
-      <a class="btn ordini text-center" href="{{route('admin.orders.index')}}">ORDINI
-      </a>
-      <a class="btn menu text-center" href="{{ route('admin.dishes.index') }}">
-        MENU
-      </a>
-      <a class="btn edit text-center" href="{{ route('admin.restaurants.edit', $restaurant->id) }}">MODIFICA RISTORANTE</a>
-    </div>
-
-
-    <form action="{{ route('admin.restaurants.destroy', $restaurant->id) }}" method="POST">
-      @csrf
-      @method('DELETE')
-      <input class="btn delete" type="submit" value="Cancella Ristorante">
-    </form>
-    @empty
-    <div class="empty">
-      <a class="btn create text-center" href="{{route('admin.restaurants.create') }}">
-      CREA IL TUO RISTORNATE
-    </a>
   </div>
+
+
 
     @endforelse
 
-
-
-
-
-
-
-<!--
-    <div class="row justify-content-center">
-      <div class="col-md-8">
-        <div class="card"> -->
-          <!-- <div class="card-header">{{ __('Dashboard') }}</div>
-          {{-- <button><a href="{{ route('admin.restaurants.create') }}">Crea Nuovo Ristorante</a></button> --}} -->
-
-
-          <!-- {{-- <div class="card-body">
-            @if (session('status'))
-            <div class="alert alert-success" role="alert">
-              {{ session('status') }}
-            </div>
-            @endif
-            {{ __('You are logged in!') }}
-          </div> --}} -->
-
-          <!-- @forelse ($user->restaurants as $restaurant)
-          <h3>{{ $restaurant->name }}</h3>
-          <button><a href="{{ route('admin.orders.index') }}">ORDINI</a></button>
-          <button><a href="{{ route('admin.dishes.index') }}">PIATTI</a></button>
-          <button><a href="{{ route('admin.restaurants.edit', $restaurant->id) }}">Modifica Ristorante</a></button>
-          <form action="{{ route('admin.restaurants.destroy', $restaurant->id) }}" method="POST">
-            @csrf
-            @method('DELETE')
-            <input type="submit" value="Cancella Ristorante">
-          </form>
-          @empty
-          <button><a href="{{ route('admin.restaurants.create') }}">Crea Nuovo Ristorante</a></button>
-          @endforelse -->
-        </div>
-      </div>
-    </div>
-
-
-
-  </div>
 </main>
 @endsection
