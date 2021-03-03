9f6bda50505bf24a096ffd318d2888cbe950a4bc
Marco Giulietti <ga6t00n@gmail.com>
2021-03-03 12:33:39 +0100

    Page 404
    Piccolo abbellimento della pagina 404

--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -2,4 +2,5 @@
     <p class="disclaimer text-center">
         Deliveboo | via degli Autostoppisti Intergalattici 42, Deep Space 00000 | &copy; Team #2 Copyright. All Rights Reserved.
     </p>
+    <div class="gradient"></div>
 </footer>
